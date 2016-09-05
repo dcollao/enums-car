679fd178f29aa03f634881fc268d5f8aadeb636b
Diego  Collao <dcollao@icloud.com>
2016-09-05 02:19:49 -0300

    readme added, buttons with images

---
+++ README.md
@@ -0,0 +1,5 @@
+result:
+
+![My image](https://i.gyazo.com/bb35295bc959312103f5ae8ed38bf062.gif)
+
+
--- enums-and-ugliest-cars/ViewController.swift
+++ enums-and-ugliest-cars/ViewController.swift
@@ -9,10 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum Cars: Int {
+        case PONTIAC = 0
+        case SUBARU = 1
+        case BMW = 2
+        case FORD = 3
+    }
 
+    
+    @IBOutlet weak var theLabel: UILabel!
+    
+    @IBAction func OnButtonTapped(sender: UIButton!) {
+        
+        if sender.tag == Cars.BMW.rawValue {
+            theLabel.text = "BMW IS AWESOME! :)"
+        } else if sender.tag == Cars.FORD.rawValue {
+            theLabel.text = "I'm a mistake of God haha T_T"
+        } else if sender.tag == Cars.PONTIAC.rawValue {
+            theLabel.text = "Damn, my designer is Leesin X_X"
+        } else {
+            theLabel.text = "Somebody shoot me now ;("
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        /*
+        var car1 = Cars.BMW
+        var car2 = Cars.FORD
+        
+        //THIS COMPARE the content of the var  -> the string <-
+        
+        
+        if car1.rawValue == car2.rawValue {
+            
+        }
+        
+        //THIS COMPARE the name of the var
+        
+        if car1 == car2 {
+            
+        }
+        */
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
